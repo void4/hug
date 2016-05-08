0962b39994c70c7b4532279fa19471fedd291d94
void <void@119.io>
2016-05-08 16:50:39 +0200

    Removed second endpoint

--- examples/pay_world.py
+++ examples/pay_world.py
@@ -5,8 +5,3 @@ pay = hug.payment.channel("0x3b63b366a72e5742b2aaa13a5e86725ed06a68f3")
 @hug.get("/pay", requires=pay("1 wei"))
 def pay_world(payment: hug.directives.payment):
     return "You paid {0} for this.".format(payment)
-
-
-@hug.get("/pay4", requires=pay(4))
-def pay_world():
-    return "You paid for this."
